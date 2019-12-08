32ea1f98e7a6fa805dcf17738b9f2abf01ed887f
GStudenikin <bogart250595@gmail.com>
2019-12-08 15:10:34 +0700

    initial commit
    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style/style.scss";
+import "./components/logo/logo.scss";
+import "./components/checklist/checklist.scss";
+import "./components/radio/radio.scss";
+import "./components/toggle/toggle.scss";
+import "./components/text-field/text-field.scss";
