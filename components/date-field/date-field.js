251b407afbd662ae3ea830202da79896ead9c57f
GStudenikin <bogart250595@gmail.com>
2020-08-26 15:23:57 +0700

    Update date-field.js

--- components/date-field/date-field.js
+++ components/date-field/date-field.js
@@ -1,9 +1,17 @@
 import Cleave from "cleave.js";
 
+var today = new Date();
+
 document.addEventListener("DOMContentLoaded", () => {
   var cleave = new Cleave(".date-field__input", {
     date: true,
     datePattern: ["d", "m", "Y"],
     delimiter: ".",
+    dateMax:
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate(),
   });
 });
