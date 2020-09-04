15d0d04cb375e76bcb306fc755bc5851447ce2d8
GStudenikin <bogart250595@gmail.com>
2020-09-04 21:10:53 +0700

    Refactoring calendar.js

--- components/calendar/calendar.js
+++ components/calendar/calendar.js
@@ -3,6 +3,7 @@ const todayDate = date.getDate();
 const todayMonth = date.getMonth();
 const todayYear = date.getFullYear();
 const calendar = document.querySelector(".calendar");
+const days = document.querySelector(".calendar__days-list");
 
 const monthNames = [
   "Январь",
@@ -18,6 +19,74 @@ const monthNames = [
   "Ноябрь",
   "Декабрь",
 ];
+
+//get current day attribute (day, month, year)
+var month = todayMonth;
+var year = todayYear;
+var dayInput = null;
+var selectedDate = null;
+
+document.querySelector(".calendar__month__current").innerHTML =
+  monthNames[month] + " " + year;
+buildMonth(month, year);
+
+calendar.addEventListener("click", function (t) {
+  if ("calendar__days-list__day" == t.target.classList) {
+    dayInput = calendar.querySelector(".calendar__days-list__day-selected");
+    if (dayInput != null) {
+      dayInput.classList.remove("calendar__days-list__day-selected");
+      dayInput.classList.add("calendar__days-list__day");
+      selectedDate = [parseInt(t.target.innerHTML), month, year];
+    }
+    dayInput = t.target;
+    t.target.classList.remove("calendar__days-list__day");
+    t.target.classList.add("calendar__days-list__day-selected");
+    selectedDate = [parseInt(t.target.innerHTML), month, year];
+    console.log(selectedDate);
+  }
+
+  if ("calendar__month__btn" == t.target.classList) {
+    if (t.target.id == "next-month") {
+      month = (month + 1) % 12;
+      if (month == 0) {
+        year++;
+      }
+      buildMonth(month, year);
+    } else if (t.target.id == "prev-month") {
+      month = (month - 1 + 12) % 12;
+      if (month == 11) {
+        year--;
+      }
+      document.getElementsByClassName("calendar__month__current")[0].innerHTML =
+        monthNames[month] + " " + year;
+      buildMonth(month, year);
+    }
+  }
+
+  if ("calendar__clear-btn" == t.target.classList) {
+    if (
+      selectedDate != null &&
+      month == todayMonth &&
+      year == todayYear &&
+      month == selectedDate[1] &&
+      year == selectedDate[2]
+    ) {
+      dayInput = calendar.querySelector(".calendar__days-list__day-selected");
+      dayInput.classList.remove("calendar__days-list__day-selected");
+      dayInput.classList.add("calendar__days-list__day");
+      month = todayMonth;
+      year = todayYear;
+      dayInput = null;
+      selectedDate = null;
+    } else {
+      month = todayMonth;
+      year = todayYear;
+      dayInput = null;
+      selectedDate = null;
+      buildMonth(month, year);
+    }
+  }
+});
 
 function amountDays(month, year) {
   switch (month) {
@@ -47,7 +116,6 @@ function amountDays(month, year) {
 }
 
 function prevMonthDays(year, month) {
-  let days = document.querySelector(".calendar__days-list");
   let firstDay = new Date(year, month, 1);
   for (let i = 1; i <= (firstDay.getDay() + 6) % 7; i++) {
     const day_element = document.createElement("div");
@@ -57,7 +125,6 @@ function prevMonthDays(year, month) {
     } else {
       var amount_days_prev = amountDays(month - 1, year);
     }
-    console.log(month + " " + amount_days_prev + " " + firstDay.getDay());
     day_element.textContent =
       amount_days_prev - (((firstDay.getDay() + 6) % 7) - i);
     days.appendChild(day_element);
@@ -66,7 +133,6 @@ function prevMonthDays(year, month) {
 
 function nextMonthDays(month, year) {
   let amount_days = amountDays(month, year);
-  let days = document.querySelector(".calendar__days-list");
   let lastDay = new Date(year, month, amount_days);
   if (lastDay.getDay() > 0) {
     let d = lastDay.getDay();
@@ -83,10 +149,12 @@ function nextMonthDays(month, year) {
 }
 
 function buildMonth(month, year) {
-  let amount_days = amountDays(month, year);
+  var amount_days = amountDays(month, year);
   let days = document.querySelector(".calendar__days-list");
   days.innerHTML = "";
 
+  document.getElementsByClassName("calendar__month__current")[0].innerHTML =
+    monthNames[month] + " " + year;
   prevMonthDays(year, month);
 
   for (let i = 1; i <= amount_days; i++) {
@@ -94,7 +162,16 @@ function buildMonth(month, year) {
     if (i == todayDate && month == todayMonth) {
       day_element.classList.add("calendar__days-list__day-current");
     } else {
-      day_element.classList.add("calendar__days-list__day");
+      if (
+        selectedDate != null &&
+        selectedDate[0] == i &&
+        selectedDate[1] == month &&
+        selectedDate[2] == year
+      ) {
+        day_element.classList.add("calendar__days-list__day-selected");
+      } else {
+        day_element.classList.add("calendar__days-list__day");
+      }
     }
     day_element.textContent = i;
     days.appendChild(day_element);
@@ -102,50 +179,3 @@ function buildMonth(month, year) {
 
   nextMonthDays(month, year);
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  //get current day attribute (day, month, year)
-  var month = todayMonth;
-  var year = todayYear;
-  var dayInput = null;
-
-  document.querySelector(".calendar__month__current").innerHTML =
-    monthNames[month] + " " + year;
-  buildMonth(month, year);
-
-  calendar.addEventListener("click", function (t) {
-    if ("calendar__days-list__day" == t.target.classList) {
-      console.log(typeof dayInput);
-      if (dayInput != null) {
-        dayInput.classList.remove("calendar__days-list__day-selected");
-        dayInput.classList.add("calendar__days-list__day");
-      }
-      dayInput = t.target;
-      t.target.classList.remove("calendar__days-list__day");
-      t.target.classList.add("calendar__days-list__day-selected");
-      t.target.nextSibling.classList.remove("calendar__days-list__day");
-    }
-
-    if ("calendar__month__btn" == t.target.classList) {
-      if (t.target.id == "next-month") {
-        month = (month + 1) % 12;
-        if (month == 0) {
-          year++;
-        }
-        document.getElementsByClassName(
-          "calendar__month__current"
-        )[0].innerHTML = monthNames[month] + " " + year;
-        buildMonth(month, year);
-      } else if (t.target.id == "prev-month") {
-        month = (month - 1 + 12) % 12;
-        if (month == 11) {
-          year--;
-        }
-        document.getElementsByClassName(
-          "calendar__month__current"
-        )[0].innerHTML = monthNames[month] + " " + year;
-        buildMonth(month, year);
-      }
-    }
-  });
-});
